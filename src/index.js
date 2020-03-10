aada601e4ecbff41dac1871ccabefe4958adc0e4
yuehhan <51277751+yuehhan@users.noreply.github.com>
2020-03-10 15:03:53 -0400

    Revert "Add react"

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-import App from "./App";
-import React from "react";
-import ReactDOM from "react-dom";
-
-ReactDOM.render(<App />, document.getElementById("root"));
+(() => {
+  console.log("[SHOPRITE-FE-DEBUGGING]");
+})(document);
